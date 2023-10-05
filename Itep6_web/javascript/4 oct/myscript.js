fc2bd968d0d887bd0aa4dc65e58cc4af95a53c66
Gaurav Kothari <87779512+kotharigaurav6@users.noreply.github.com>
2023-10-05 18:07:43 +0530

    Add files via upload

---
+++ Itep6_web/javascript/4 oct/myscript.js
@@ -0,0 +1,4 @@
+
+        document.write("<br>This is an example of javascript");
+        document.write("<br><span style='color:red'>This is an example of javascript</span>");
+        document.write("<br>This is an example of javascript");
